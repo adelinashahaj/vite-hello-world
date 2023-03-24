48f7004fcc3f2a0572d50f00c62019c78df28f53
adelinashahaj <adelinarucaj@icloud.com>
2023-03-24 16:36:48 +0100

    esercizio concluso + bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <img src="../../public/jumbotron.png" alt="">
+    </div>
+
+
+</template>
+
+
+
+<script>
+
+</script>
+
+
+<style>
+.container{
+    max-width: 1000px;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+}
+
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+
+
+</template>
+
+
+
+<script>
+
+</script>
+
+
+<style>
+
+h1{
+    text-align: center;
+    color: brown;
+}
+</style>
